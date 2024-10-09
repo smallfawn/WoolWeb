516e1139c4f5d9c35578299bf70161afe347e5f3
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-10-09 13:31:20 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'client/src'),
     }
   },
   server: {
